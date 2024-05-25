986ac20ef31c24aef231574577b8862dc7505ecd
Hanqing Huang <hqh@apple.com>
2024-05-24 17:07:09 -0700

    improve animation for long decl list
    improve animation for long decl list:
    1. bump to top of page first
    2. decrease wait time

--- src/components/DocumentationTopic/PrimaryContent/DeclarationList.vue
+++ src/components/DocumentationTopic/PrimaryContent/DeclarationList.vue
@@ -161,8 +161,9 @@ export default {
       if (identifier === this.identifier) return;
       this.selectedIdentifier = identifier;
       await this.$nextTick(); // wait for identifier to update
+      window.scrollTo(0, 0);
       this.isExpanded = false; // collapse the list
-      await waitFor(500); // wait for animation to finish
+      await waitFor(300); // wait for animation to finish
       const url = buildUrl(this.references[identifier].url, this.$route.query);
       this.$router.push(url);
     },
